7757086cf978ab34359320f5c08a2b282b62c0fc
DavideAvagnano <dav.avagnano@gmail.com>
2025-01-03 08:30:27 +0100

    Add first endpoint, add cors to enables cross-origin requests, made frist request in client

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,17 @@
 import express from "express";
+import cors from "cors";
+
+import planetsRouter from "./routes/planets-router";
 
 export const app = express();
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+// ROUTES
+app.use("/", planetsRouter);
