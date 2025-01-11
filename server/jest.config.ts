9eae7ed4f2c1de27af475af567290e8e162b57b2
DavideAvagnano <dav.avagnano@gmail.com>
2025-01-11 10:35:56 +0100

    Update tast with mongodb connection

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -1,14 +1,10 @@
-module.exports = {
+export default {
   preset: "ts-jest", // Use the ts-jest preset to support TypeScript
   testEnvironment: "node", // Set the test environment to Node.js
   roots: ["<rootDir>"], // The folder where test files are located
+  // Transform TypeScript files using ts-jest and use the tsconfig.json for TypeScript configuration
   transform: {
-    "^.+\\.ts$": "ts-jest", // Transform TypeScript files using ts-jest
+    "^.+\\.ts$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.json" }],
   },
   moduleFileExtensions: ["ts", "js", "json", "node"], // Supported file extensions
-  globals: {
-    "ts-jest": {
-      tsconfig: "<rootDir>/tsconfig.json", // Use the tsconfig.json for TypeScript configuration
-    },
-  },
 };
